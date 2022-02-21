9da0a0db7409eb3ae86aa6c0d37f72c7093b1a58
Akmal Kadirov <akmal@kadirov.org>
2022-02-21 12:38:51 +0500

    edit (ParameterGetter) replace SymfonyContainer with PsrContainer

--- src/Component/Core/ParameterGetter.php
+++ src/Component/Core/ParameterGetter.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Kadirov\Component\Core;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 
 class ParameterGetter
 {
@@ -17,7 +18,7 @@ class ParameterGetter
 
     public function get(string $name): mixed
     {
-        return $this->container->getParameter($name);
+        return $this->getParameter($name);
     }
 
     public function getString(string $name): string
@@ -43,5 +44,14 @@ class ParameterGetter
     public function getFloat(string $name): float
     {
         return (float)$this->get($name);
+    }
+
+    protected function getParameter(string $name): mixed
+    {
+        if (!$this->container->has('parameter_bag')) {
+            throw new ServiceNotFoundException('parameter_bag.', null, null, [], sprintf('The "%s::getParameter()" method is missing a parameter bag to work properly. Did you forget to register your controller as a service subscriber? This can be fixed either by using autoconfiguration or by manually wiring a "parameter_bag" in the service locator passed to the controller.', static::class));
+        }
+
+        return $this->container->get('parameter_bag')->get($name);
     }
 }
