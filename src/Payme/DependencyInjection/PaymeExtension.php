af5b28672b2ca922d128e355c5d249e790839617
Ma'ruf Sheraliyev <mfsv1011@mail.ru>
2025-02-17 12:31:36 +0500

    fix (exeption import)

--- src/Payme/DependencyInjection/PaymeExtension.php
+++ src/Payme/DependencyInjection/PaymeExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kadirov\Payme\DependencyInjection;
 
+use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 class PaymeExtension extends Extension
 {
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
